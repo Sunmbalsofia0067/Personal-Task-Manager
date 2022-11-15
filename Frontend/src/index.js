14eae5428f2c0ab8f419feeab350aa8c9f994166
talhacodes <talhacodes@gmail.com>
2022-11-15 13:50:49 +0500

    Added dragable functionality to the tasks

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -7,11 +7,9 @@ import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <App />
     </MantineProvider>
-  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
--- Frontend/src/pages/Homepage/Homepage.js
+++ Frontend/src/pages/Homepage/Homepage.js
@@ -1,6 +1,164 @@
-function Homepage() {
-    return ( 
-        <h1>Hello Sofia</h1>
-     );
+import { useState } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+const grid = 8
+
+const getItems = (count, offset = 0) =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${Math.random()*100000 + offset}`,
+    content: `item ${k + offset}`
+  }))
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+})
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250
+})
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
 }
-export default Homepage;
+
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source)
+  const destClone = Array.from(destination)
+  const [removed] = sourceClone.splice(droppableSource.index, 1)
+
+  destClone.splice(droppableDestination.index, 0, removed)
+
+  const result = {}
+  result[droppableSource.droppableId] = sourceClone
+  result[droppableDestination.droppableId] = destClone
+
+  return result
+}
+
+function Homepage () {
+  const [items, setItems] = useState(getItems(10))
+  const [selectedItems, setSelectedItems] = useState(getItems(5))
+
+  const id2List = {
+    todos: 'items',
+    done: 'selectedItems'
+  }
+  const getList = id => {
+    const list = id2List[id]
+    return list === 'items' ? items : selectedItems
+  }
+
+  const onDragEnd = result => {
+    const { source, destination } = result;
+
+    console.log(result);
+
+    // dropped outside the list
+    if (!destination) {
+      return
+    }
+
+    if (source.droppableId === destination.droppableId) {
+      const items = reorder(
+        getList(source.droppableId),
+        source.index,
+        destination.index
+      )
+
+      if(source.droppableId === 'todos'){
+        setItems(items)
+      }
+
+      if (source.droppableId === 'done') {
+        setSelectedItems(items)
+      }
+    } else {
+      const result = move(
+        getList(source.droppableId),
+        getList(destination.droppableId),
+        source,
+        destination
+      )
+
+      console.log(result);
+      setItems(result.todos)
+      setSelectedItems(result.done)
+    }
+  }
+  return (
+    <div style={{ display: 'flex' }}>
+    <DragDropContext onDragEnd={onDragEnd}>
+    <Droppable droppableId='todos'>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          style={getListStyle(snapshot.isDraggingOver)}
+        >
+          {items.map((item, index) => (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                  )}
+                >
+                  {item.content}
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+    <Droppable droppableId='done'>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          style={getListStyle(snapshot.isDraggingOver)}
+        >
+          {selectedItems.map((item, index) => (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                  )}
+                >
+                  {item.content}
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  </DragDropContext>
+    </div>
+  )
+}
+export default Homepage
